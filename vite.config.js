602b7f6bec58372e611b1d30c0aacf628e4e1d10
IsaacPH2005 <parisacahuallpaisaac@gmail.com>
2024-12-20 13:54:06 -0400

    bug base 2

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,16 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
   ],
- /*  base:'https://isaacph2005.github.io/hamilo_web/', */
+  base: '/hamilo_web/', // Asegúrate de que esta sea la ruta correcta
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-})
+});
